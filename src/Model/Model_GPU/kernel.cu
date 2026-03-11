62df52bdb5c1ce6486212813612105b3dc483e35
Borishkof <vikingmazout@gmail.com>
2026-03-11 15:35:54 +0100

    [Feat] basic shared memory for gpu

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -3,38 +3,71 @@
 #include "cuda.h"
 #include "kernel.cuh"
 
-__global__ void compute_acc(float3* positionsGPU, float3* velocitiesGPU, 
-                             float3* accelerationsGPU, float* massesGPU, int n_particles)
+__global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
+    const int block_size = 128; 
+
+    __shared__ float3 shared_pos[block_size];
+    __shared__ float  shared_mass[block_size];
+
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i >= n_particles)
-		return;
+    float3 acc = {0.0f, 0.0f, 0.0f};
+    float3 my_pos;
 
-    float ai_x = 0.f, ai_y = 0.f, ai_z = 0.f;
-
-    for (int j = 0; j < n_particles; j++)
-    {
-        float dx = positionsGPU[j].x - positionsGPU[i].x;
-        float dy = positionsGPU[j].y - positionsGPU[i].y;
-        float dz = positionsGPU[j].z - positionsGPU[i].z;
-
-        float d2 = dx*dx + dy*dy + dz*dz;
-
-        float force;
-        if (d2 <  1.f)
-			force = 10.f;
-        else
-			force = 10.f * rsqrtf(d2*d2*d2);
-
-        float fm = force * massesGPU[j];
-        ai_x += dx * fm;
-        ai_y += dy * fm;
-        ai_z += dz * fm;
+    if(i < n_particles) {
+        my_pos = positionsGPU[i];
     }
 
-    accelerationsGPU[i].x = ai_x;
-    accelerationsGPU[i].y = ai_y;
-    accelerationsGPU[i].z = ai_z;
+    // On parcourt toutes les particules j par tiles
+    for (int tile = 0; tile < n_particles; tile += block_size)
+    {
+        // Chaque thread du bloc
+        int target_j = tile + threadIdx.x;
+        if (target_j < n_particles) {
+            shared_pos[threadIdx.x]  = positionsGPU[target_j];
+            shared_mass[threadIdx.x] = massesGPU[target_j];
+        } else {
+            shared_pos[threadIdx.x]  = {0,0,0};
+            shared_mass[threadIdx.x] = 0.0f;
+        }
+
+        // Bloc chargé
+        __syncthreads();
+
+        // CALCUL dans le tile
+        if (i < n_particles) {
+            for (int j = 0; j < block_size; j++) {
+                if (i != (tile + j)) {
+                    float3 pos_j = shared_pos[j];
+                    float diffx = pos_j.x - my_pos.x;
+                    float diffy = pos_j.y - my_pos.y;
+                    float diffz = pos_j.z - my_pos.z;
+
+                    float dist_sq = diffx * diffx + diffy * diffy + diffz * diffz;
+                    
+                    float dij;
+                    if (dist_sq < 1.0f) {
+                        dij = 10.0f;
+                    } else {
+                        // Utilisation de rsqrtf pour la performance
+                        float inv_dist = rsqrtf(dist_sq);
+                        dij = 10.0f * (inv_dist * inv_dist * inv_dist);
+                    }
+
+					dij *= shared_mass[j];
+
+                    acc.x += diffx * dij;
+                    acc.y += diffy * dij;
+                    acc.z += diffz * dij;
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    if(i < n_particles) {
+        accelerationsGPU[i] = acc;
+    }
 }
 
 __global__ void maj_pos(float3* positionsGPU, float3* velocitiesGPU, 
